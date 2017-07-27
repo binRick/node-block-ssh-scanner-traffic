d8b489b0c9ca01c795702660d4fb6451c51b9956
rick <rick@ricks-Air.cfl.rr.com>
2017-07-26 20:29:30 -0400

    tweaks

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    scannerIntervalSeconds: 10,
-    scannerThreshold: 15,
+    scannerIntervalSeconds: 30,
+    scannerThreshold: 10,
     interval: 5,
     localNetworks: ['66.35.64.0/19', '199.119.180.0/22', '208.84.144.0/21', '208.77.96.0/21'],
     pmacctd: '/usr/local/sbin/pmacctd',
